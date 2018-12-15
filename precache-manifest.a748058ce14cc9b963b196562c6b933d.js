8ab9aa736dd9c5313bdddd3d324639c98815c057
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-12-14 23:45:18 -0500

    Updates

--- precache-manifest.a748058ce14cc9b963b196562c6b933d.js
+++ precache-manifest.a748058ce14cc9b963b196562c6b933d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lim-2018-11-bc-core-am-data-lovers/static/js/runtime~main.5553abda.js"
   },
   {
-    "revision": "7f91a78fdd28c3f8a089",
-    "url": "/lim-2018-11-bc-core-am-data-lovers/static/js/main.7f91a78f.chunk.js"
+    "revision": "8f314101fc796f3b3aa6",
+    "url": "/lim-2018-11-bc-core-am-data-lovers/static/js/main.8f314101.chunk.js"
   },
   {
     "revision": "bfa01eff9b16fc53431d",
     "url": "/lim-2018-11-bc-core-am-data-lovers/static/js/1.bfa01eff.chunk.js"
   },
   {
-    "revision": "7f91a78fdd28c3f8a089",
-    "url": "/lim-2018-11-bc-core-am-data-lovers/static/css/main.a880cc1f.chunk.css"
+    "revision": "8f314101fc796f3b3aa6",
+    "url": "/lim-2018-11-bc-core-am-data-lovers/static/css/main.45256153.chunk.css"
   },
   {
-    "revision": "aef391891dfb1856fd2daf344e5c445e",
+    "revision": "44454e6394e9850eac2faf760ecd2e3e",
     "url": "/lim-2018-11-bc-core-am-data-lovers/index.html"
   }
 ];
