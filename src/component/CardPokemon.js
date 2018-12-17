62ffa5713f4f44f9b7c6d8d3932c5fb95642e56b
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-12-17 11:55:44 -0500

    Agregando etiqueta figure para img

--- src/component/CardPokemon.js
+++ src/component/CardPokemon.js
@@ -5,7 +5,9 @@ const CardPokemon = ({ name, img, updatePokemon, num }) => {
   return (
     <div className="col-6 col-sm-3 col-lg-2 text-center mb-2 mb-md-3 pr-2 pl-2 pr-md-3 pl-md-3">
       <Link to={`/${num}`} onClick={() => updatePokemon(num)} className="card text-dark">
-        <img src={img} alt={name} className="img-fluid m-2" />
+        <figure className="m-0">
+          <img src={img} alt={name} className="img-fluid m-2" />
+        </figure>
         <h5>{name}</h5>
       </Link>
     </div>
